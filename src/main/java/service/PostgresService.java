30b642c10cb7cabd7071ed770124d1d61e9b9c2e
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-05-18 20:09:11 -0300

    adiciona bean de datasource no servico do postgres

--- src/main/java/service/PostgresService.java
+++ src/main/java/service/PostgresService.java
@@ -1,8 +1,16 @@
 package service;
 
+import io.agroal.api.AgroalDataSource;
+import io.quarkus.runtime.Startup;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
+@Startup
 public class PostgresService {
+
+    @Inject
+    AgroalDataSource defaultDatasource;
 
 }

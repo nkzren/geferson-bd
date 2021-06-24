823d737e20e793fe32acdae197a53de9d0f00d40
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-06-23 22:28:39 -0300

    adiciona uma trigger do artefato A da parte II

---
+++ docs/Parte II/Artefato_A/artefato_A_1.sql
@@ -0,0 +1,15 @@
+CREATE FUNCTION deletar_avaliacoes_usuario() 
+   RETURNS TRIGGER 
+   LANGUAGE PLPGSQL
+AS $$
+BEGIN
+	DELETE FROM avaliacao a
+	WHERE a.usuario_id = NEW.id;
+END;
+$$
+
+CREATE TRIGGER trigger_deletar_avaliacoes_usuario
+    AFTER UPDATE ON usuario
+    FOR EACH ROW
+    WHEN (NEW.deletado = TRUE)
+    EXECUTE PROCEDURE deletar_avaliacoes_usuario();

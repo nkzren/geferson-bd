fc324020e3a6a5d5fd94b9956f484b9bb551dd2c
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-06-23 22:32:19 -0300

    adiciona sql padrao na trigger de deletar avaliacoes

--- docs/Parte II/Artefato_A/artefato_A_1.sql
+++ docs/Parte II/Artefato_A/artefato_A_1.sql
@@ -13,3 +13,13 @@ CREATE TRIGGER trigger_deletar_avaliacoes_usuario
     FOR EACH ROW
     WHEN (NEW.deletado = TRUE)
     EXECUTE PROCEDURE deletar_avaliacoes_usuario();
+
+-- SQL Padrão
+CREATE TRIGGER trigger_deletar_avaliacoes_usuario
+    AFTER UPDATE ON usuario
+    FOR EACH ROW
+    WHEN (NEW.deletado = TRUE)
+    BEGIN
+        DELETE FROM avaliacao a
+        WHERE a.usuario_id = NEW.id;
+    END;

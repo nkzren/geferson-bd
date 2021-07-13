24ed83f06cb48256d870c0cfd4fd9ecca0a6384f
nkzren <renan.nakazawa0@gmail.com>
2021-07-13 17:49:33 -0300

    adiciona ; no final do ultimo insert

--- populate-script/main.py
+++ populate-script/main.py
@@ -6,12 +6,12 @@ from functions import random_date
 def insert_users(f, qtd):
     f.write("INSERT INTO usuario (nickname, email, data_nascimento) VALUES\n")
 
-    for _ in range(qtd):
+    for i in range(qtd):
         nickname = f'{names.get_first_name()}.{names.get_last_name()}'.lower()
         data_nascimento = random_date("1970/01/01", "2009/01/01", random.random())
 
         print(f"('{nickname}', '{nickname}@gmail.com', '{data_nascimento}'),")
-        f.write(f"('{nickname}', '{nickname}@gmail.com', '{data_nascimento}'),\n")
+        f.write(f"('{nickname}', '{nickname}@gmail.com', '{data_nascimento}') %("," if i == qtd else ";" )\n")
     
 
 

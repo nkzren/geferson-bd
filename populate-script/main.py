9c1c68186b1ee6ab00e9d5a551d6a2ad53f4f97c
Stefany <stefanyramsdorf@gmail.com>
2021-07-13 18:41:32 -0300

    adiciona script de inseercao de ator

--- populate-script/main.py
+++ populate-script/main.py
@@ -5,19 +5,22 @@ from functions import random_date
 
 def insert_users(f, qtd):
     f.write("INSERT INTO usuario (nickname, email, data_nascimento) VALUES\n")
-
     for i in range(qtd):
         nickname = f'{names.get_first_name()}.{names.get_last_name()}'.lower()
         data_nascimento = random_date("1970/01/01", "2009/01/01", random.random())
-
-        print(f"('{nickname}', '{nickname}@gmail.com', '{data_nascimento}'),")
+        # print(f"('{nickname}', '{nickname}@gmail.com', '{data_nascimento}'),")
         f.write(f"('{nickname}', '{nickname}@gmail.com', '{data_nascimento}'){',' if i < qtd-1 else ';' }\n")
     
+def insert_ator(f, qtd):
+    f.write("INSERT INTO ator (id, nome) VALUES\n")
 
+    for i in range(qtd):
+        nome = names.get_full_name()
+        f.write(f"('{nome}'){',' if i < qtd-1 else ';' }\n"))
 
 
 f = open('populate-script/script.sql', 'a')
-insert_users(f, 5)
+insert_users(f, 10000)
 
 print(names.get_first_name())
 print(random_date("1970/01/01", "2009/01/01", random.random()))

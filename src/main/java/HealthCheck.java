d0cf831fd644e19e4239d4489d1c220299786567
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-07-24 16:52:12 -0300

    corrige healthchecks

--- src/main/java/HealthCheck.java
+++ src/main/java/HealthCheck.java
@@ -1,14 +1,22 @@
+import io.agroal.api.AgroalDataSource;
 import io.quarkus.runtime.Startup;
-import model.TestDump;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.SQLException;
 
 @ApplicationScoped
 @Startup
 @Path("health")
+@Produces(MediaType.TEXT_PLAIN)
 public class HealthCheck {
+
+    @Inject
+    AgroalDataSource defaultDataSource;
 
     @GET
     public String health() {
@@ -17,8 +25,8 @@ public class HealthCheck {
 
     @GET
     @Path("db")
-    public String dbHealth() {
-        return TestDump.testResource().test;
+    public String dbHealth() throws SQLException {
+        return String.valueOf(defaultDataSource.getConnection().isValid(0));
     }
 
 }
